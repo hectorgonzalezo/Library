b0be94bf6caa6d1e42e923b52a52ba380c8bee0b
Hector Gonzalez <gorozco.hector@gmail.com>
2022-08-26 17:05:02 -0400

    Refactored Book object to class

--- scripts/script.js
+++ scripts/script.js
@@ -1,117 +1,183 @@
-const root = document.querySelector('html')
-const body = document.querySelector('#page-body')
-const librarySection = document.querySelector('main')
-const darkModeSwitch = document.querySelector('#dark-mode')
-const darkModeSwitchDiv = document.querySelector('#dark-mode-switch-div')
-const newBookButton = document.querySelector('#book-button')
-const popUp = document.querySelector('#pop-up')
-const addBookButton = document.querySelector('#add-book-button')
-const closePopUpButton = document.querySelector('#close-pop-up')
-const addBookForm = document.querySelector('#add-book-form')
 
-//This object will contain all the books
-let myLibrary = {}
+root = document.querySelector('html');
+body = document.querySelector('#page-body');
+librarySection = document.querySelector('main');
+darkModeSwitch = document.querySelector('#dark-mode');
+darkModeSwitchDiv = document.querySelector('#dark-mode-switch-div');
+newBookButton = document.querySelector('#book-button');
+popUp = document.querySelector('#pop-up');
+addBookButton = document.querySelector('#add-book-button');
+closePopUpButton = document.querySelector('#close-pop-up');
+addBookForm = document.querySelector('#add-book-form');
 
-//object constructor for books
-function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-}
 
-//stores book in myLibrary object
-Book.prototype.addToLibrary = function() {
-    myLibrary[this.title] = this
-}
+//displays all books in list
+function displayAllBooks(library = Book.myLibrary) {
+    Object.keys(Book.myLibrary).forEach((book) => {
+        library[book].display();
+    }
+    )
+};
 
-//Adds book into into a table, appends it to DOM and displays it on webpage.
-Book.prototype.display = function() {
-   //adds divs with book information to library section
-   const bookDiv = document.createElement('div');
-   bookDiv.classList.add('book');
+function togglePopup() {
+    //clear form
+    addBookForm.reset();
 
-   //creates table
-   const bookTable = document.createElement('table')
-   bookDiv.appendChild(bookTable)
-   const tableBody = document.createElement('tbody')
-   bookTable.appendChild(tableBody)
-   tableBody.classList.add('book-table')
-   for (const property in this) {
-       if (this.hasOwnProperty(property)){//dont display prototype properties
-       //create table row
-       const tableRow = document.createElement('tr');
-       tableBody.appendChild(tableRow)
-       //format property to be suitable for display
-       const formattedProperty = `${capitalize(property)}:`;
-       //add info to tableRow
-       [formattedProperty, this[property]].forEach((text) => {
-           if (text != 'Title:') {
-               const tableData = document.createElement('td');
-               //format "Read" booleans to image
-               if (typeof (text) != 'boolean') {
-                   tableData.innerText = text;
-               } else {
-                   this.addReadButton(tableData)
-               }
-               tableRow.appendChild(tableData)
-           }
-       }
-       )
-   }
-   }
-   this.addDeleteButton(bookDiv)
-
-   librarySection.prepend(bookDiv)
-}
-
-Book.prototype.addDeleteButton = function(location) {
-    //make space for delete button
-    const trashBin = document.createElement('span')
-    trashBin.classList.add('trash-bin')
-    location.appendChild(trashBin)
-    //add button functionality
-    trashBin.addEventListener('click', () => {
-        this.remove(location)
-    })
-}
-
-Book.prototype.remove = function(book) {
-    //remove from Dom
-    librarySection.removeChild(book);
-    //remove from myLibrary object
-    delete myLibrary[this['title']]
-}
-
-Book.prototype.addReadButton = function (location) {
-    //make space for delete button
-     const imageRead = document.createElement('div');
-     imageRead.classList.add('image-read')
-     this.read ? imageRead.classList.add('read-it') : null;
-     location.appendChild(imageRead)
-    //add button functionality
-    imageRead.addEventListener('click', (e) => {
-        imageRead.classList.toggle('read-it');
-        this.read = this.read ? false : true;//toggle read status on object with button
-    }) 
- }
-
-//correct value data types for number of pages and read.
-Book.prototype.correctData = function () {
-    this.pages = parseInt(this.pages) //to number
-    //to boolean
-    if (this.read == undefined) {
-        this.read = false;
+    popUp.classList.toggle('visible-pop-up');
+    body.classList.toggle('greyout');
+    newBookButton.classList.toggle('active-button');
+    darkModeSwitchDiv.classList.toggle('inactive-switch');
+    if (newBookButton.disabled) {
+        newBookButton.disabled = false;
+        darkModeSwitch.disabled = false;
     } else {
-        this.read = true;
-    };
-    return this
+        newBookButton.disabled = true;
+        darkModeSwitch.disabled = true;
+    }
 }
 
-//used to capitalize book properties
-function capitalize(word) {
-    return word.replace(/\w/, (letter) => letter.toUpperCase())
+function addAllListeners() {
+    //change to dark mode with button
+    darkModeSwitch.addEventListener('click', () => {
+        root.classList.toggle("dark")
+    });
+
+
+    //show popup when pressing "+" button
+    newBookButton.addEventListener('click', this.togglePopup)
+
+
+    //add book to library with button on pop-up
+    addBookButton.addEventListener('click', (e) => {
+        //prevents button from submitting
+        e.preventDefault();
+
+        if (addBookForm.checkValidity()) {
+            //extract data from form and make it a FormData
+            const formData = new FormData(addBookForm);
+            const addBookData = Object.fromEntries(formData.entries());
+            //create book from data
+            const newBook = Object.assign(new Book(), addBookData);
+
+            const correctedBook = newBook.correctData();
+
+            correctedBook.display();
+            togglePopup();
+            Book.myLibrary[correctedBook['title']] = correctedBook
+        }
+    })
+
+    closePopUpButton.addEventListener('click', () => this.togglePopup())
 }
+
+//Class for books
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    };
+    //This object will contain all the books
+    static myLibrary = {};
+
+    //used to capitalize book properties
+    static capitalize(word) {
+        return word.replace(/\w/, (letter) => letter.toUpperCase());
+    }
+
+    //stores book in myLibrary object
+    addToLibrary() {
+        Book.myLibrary[this.title] = this;
+    }
+
+
+    //Adds book into into a table, appends it to DOM and displays it on webpage.
+    display() {
+        //adds divs with book information to library section
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book');
+
+        //creates table
+        const bookTable = document.createElement('table');
+        bookDiv.appendChild(bookTable);
+        const tableBody = document.createElement('tbody');
+        bookTable.appendChild(tableBody);
+        tableBody.classList.add('book-table');
+        for (const property in this) {
+            if (this.hasOwnProperty(property)) {//dont display prototype properties
+                //create table row
+                const tableRow = document.createElement('tr');
+                tableBody.appendChild(tableRow);
+                //format property to be suitable for display
+                const formattedProperty = `${Book.capitalize(property)}:`;
+                //add info to tableRow
+                [formattedProperty, this[property]].forEach((text) => {
+                    if (text != 'Title:') {
+                        const tableData = document.createElement('td');
+                        //format "Read" booleans to image
+                        if (typeof (text) != 'boolean') {
+                            tableData.innerText = text;
+                        } else {
+                            this.addReadButton(tableData);
+                        }
+                        tableRow.appendChild(tableData);
+                    }
+                }
+                )
+            }
+        }
+        this.addDeleteButton(bookDiv);
+
+        librarySection.prepend(bookDiv);
+    }
+
+    addDeleteButton(location) {
+        //make space for delete button
+        const trashBin = document.createElement('span');
+        trashBin.classList.add('trash-bin');
+        location.appendChild(trashBin);
+        //add button functionality
+        trashBin.addEventListener('click', () => {
+            this.remove(location);
+        })
+    }
+
+    remove(book) {
+        //remove from Dom
+        librarySection.removeChild(book);
+        //remove from myLibrary object
+        delete Book.myLibrary[this['title']]
+    }
+
+    addReadButton(location) {
+        //make space for delete button
+        const imageRead = document.createElement('div');
+        imageRead.classList.add('image-read');
+        this.read ? imageRead.classList.add('read-it') : null;
+        location.appendChild(imageRead);
+        //add button functionality
+        imageRead.addEventListener('click', (e) => {
+            imageRead.classList.toggle('read-it');
+            this.read = this.read ? false : true;//toggle read status on object with button
+        })
+    }
+
+    //correct value data types for number of pages and read.
+    correctData() {
+        this.pages = parseInt(this.pages) //to number
+        //to boolean
+        if (this.read == undefined) {
+            this.read = false;
+        } else {
+            this.read = true;
+        };
+        return this
+    }
+}
+
+
+
 
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, true).addToLibrary();
@@ -120,64 +186,5 @@ const razaCosmica = new Book('La Raza Cósmica', 'José Vasconcelos', 70, true).
 const principles = new Book("The principles of Object-Oriented Javascript", 'Nicholas C. Zakas', 112, false).addToLibrary();
 
 
-//displays all books in list
-function displayAllBooks(library = myLibrary) {
-
-    Object.keys(myLibrary).forEach( (book) => {
-        console.log(library[book])
-        library[book].display()
-    }
-    )
-}
-displayAllBooks()
-
-
-
-function togglePopup() {
-    //clear form
-    addBookForm.reset();
-    
-    popUp.classList.toggle('visible-pop-up');
-    body.classList.toggle('greyout');
-    newBookButton.classList.toggle('active-button');
-    darkModeSwitchDiv.classList.toggle('inactive-switch');
-    if (newBookButton.disabled){
-        newBookButton.disabled = false;
-        darkModeSwitch.disabled = false;
-    } else {
-    newBookButton.disabled = true;
-    darkModeSwitch.disabled = true;
-    }
-}
-
-//change to dark mode with button
-darkModeSwitch.addEventListener('click', () => {
-    root.classList.toggle("dark")
-});
-
-
-//show popup when pressing "+" button
-newBookButton.addEventListener('click', togglePopup)
-
-
-//add book to library with button on pop-up
-addBookButton.addEventListener('click', (e) => {
-    //prevents button from submitting
-    e.preventDefault();
-
-    if (addBookForm.checkValidity()) {
-        //extract data from form and make it a FormData
-        const formData = new FormData(addBookForm)
-        const addBookData = Object.fromEntries(formData.entries())
-        //create book from data
-        const newBook = Object.assign(new Book(), addBookData);
-
-        const correctedBook = newBook.correctData();
-
-        correctedBook.display();
-        togglePopup()
-        myLibrary[correctedBook['title']] = correctedBook
-    }
-})
-
-closePopUpButton.addEventListener('click', () => {togglePopup()})
+displayAllBooks();
+addAllListeners();
